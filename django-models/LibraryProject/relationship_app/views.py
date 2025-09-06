fa914a78b3640f36edf493b478c294748c1b18ff
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-09-06 16:32:27 +0100

    renanmed

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views import DetailView
+from django.views.generic.detail import DetailView
 from django.template import loader
 from django.http import HttpResponse
 from .models import Book, Author, Library, Librarian
