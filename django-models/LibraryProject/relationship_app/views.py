45ea357e2802f969835e136b016fd7ead8b49895
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-09-06 16:31:08 +0100

    renanmed

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,6 +3,7 @@ from django.views import ListView, DetailView
 from django.template import loader
 from django.http import HttpResponse
 from .models import Book, Author, Library, Librarian
+from .models import Library
 
 # Create your views here.
 
@@ -16,7 +17,7 @@ def list_books(request):
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
     # def get_context_data(self, **kwargs):
