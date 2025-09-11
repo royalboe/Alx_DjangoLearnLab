230676c6a74d29bbfd95f79d57d568aeb9752a5e
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-09-11 22:09:43 +0100

    moved user model into bookshelf

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, User
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author')
     list_filter = ('publication_year',)
 
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'date_of_birth', 'is_staff')
+    search_fields = ('username', 'email')
+    list_filter = ('is_staff', 'is_active')
+
 admin.site.register(Book, BookAdmin)
+admin.site.register(User, UserAdmin)
 
 # Register your models here.
--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,45 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-class CustomUserManager(BaseUserManager):
-    def create_user(self, username, email, date_of_birth, password=None, **extra_fields):
-        if not email:
-            raise ValueError('The Email field must be set')
-        if not username:
-            raise ValueError('The Username field must be set')
-        if not date_of_birth:
-            raise ValueError('The Date of Birth field must be set')
-        
-        email = self.normalize_email(email)
-        user = self.model(
-            username=username, 
-            email=email, 
-            date_of_birth=date_of_birth, 
-            **extra_fields
-        )
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
 
-    def create_superuser(self, username, email, date_of_birth, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
+from django.contrib.auth import get_user_model
 
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
+User = get_user_model()
 
-        user = self.create_user(username, email, date_of_birth, password, **extra_fields)
-        user.is_admin = True
-        user.save(using=self._db)
-        return user
 
-class User(AbstractUser):
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
-
-    objects = CustomUserManager()
 
 # Create your models here.
 class Author(models.Model):
