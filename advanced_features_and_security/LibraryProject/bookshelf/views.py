4cfa5c1a7487fa601bd229c341c0cb7ac95cbe1d
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-09-14 01:06:36 +0100

    usinf example form

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import permission_required
 from .models import Book
-from .forms import BookForm
+from .forms import ExampleForm
 
 # Create your views here.
 
 @permission_required('bookshelf.can_create', raise_exception=True)
 def add_book(request):
     if request.method == 'POST':
-        form = BookForm(request.POST)
+        form = ExampleForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('book_list')  # redirect to your book list view
     else:
-        form = BookForm()
+        form = ExampleForm()
     return render(request, 'bookshelf/add_book.html', {'form': form})
 
 
@@ -21,12 +21,12 @@ def add_book(request):
 def edit_book(request, book_id):
     book = get_object_or_404(Book, id=book_id)
     if request.method == 'POST':
-        form = BookForm(request.POST, instance=book)
+        form = ExampleForm(request.POST, instance=book)
         if form.is_valid():
             form.save()
             return redirect('book_list')
     else:
-        form = BookForm(instance=book)
+        form = ExampleForm(instance=book)
     return render(request, 'bookshelf/edit_book.html', {'form': form, 'book': book})
 
 
@@ -41,11 +41,11 @@ def delete_book(request, book_id):
 @permission_required('bookshelf.can_view', raise_exception=True)
 def add_book(request):
     if request.method == 'POST':
-        form = BookForm(request.POST)
+        form = ExampleForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('book_list')  # redirect to your book list view
     else:
-        form = BookForm()
+        form = ExampleForm()
     return render(request, 'bookshelf/view_book.html', {'form': form})
 
