1a3e6fa264c4cbf9a2936f5ec65262d131d27708
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-09-28 23:50:14 +0100

    requirements

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -12,5 +12,7 @@ urlpatterns = [
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
     path('books/create/', BookCreateView.as_view(), name='book-create'),
     path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
+    path('books/update/', BookUpdateView.as_view(), name='book-update'),
+    path('books/delete/', BookDeleteView.as_view(), name='book-delete'),
 ]
