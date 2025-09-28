c13a6a9eaf795b5864c9b6316204a6141b01b79a
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-09-28 23:06:43 +0100

    filters and ordering

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api'
+    'api',
+    'django_filters'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ]
+}
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,12 +1,40 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
 from .serializers import BookSerializer
 
 # ListView: Handles GET /books/ to list all books
 class BookListView(generics.ListAPIView):
+    """
+    API endpoint to list all books with support for filtering, searching, and ordering.
+
+    - Filtering fields: title, author, publication_year
+    - Search fields: title, author's name
+    - Ordering fields: title, publication_year
+    """
+
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.AllowAny]  # Public access
+    permission_classes = [permissions.AllowAny]
+
+    # Enable filtering, searching, and ordering
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter
+    ]
+
+    # Fields that can be used for filtering
+    filterset_fields = ['title', 'author', 'publication_year']
+
+    # Fields that can be used for search
+    search_fields = ['title', 'author__name']
+
+    # Fields that can be used for ordering
+    ordering_fields = ['title', 'publication_year']
+
+    # Default ordering (optional)
+    ordering = ['title']
 
 # DetailView: Handles GET /books/<pk>/ to retrieve a single book
 class BookDetailView(generics.RetrieveAPIView):
