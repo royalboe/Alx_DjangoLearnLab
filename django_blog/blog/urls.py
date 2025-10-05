6a30f1bac4a4bd2ca6f78d1063cf85560be25df3
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-10-05 23:05:36 +0100

    fixing bug

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -16,8 +16,10 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('profile/', views.profile, name='profile'),
     path('posts/', views.PostListView.as_view(), name='post-list'),
-    path('posts/new/', views.PostCreateView.as_view(), name='post-create'),
+    path("post/new/", views.PostCreateView.as_view(), name='post-create'),
     path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
-    path('posts/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
-    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+    path("post/<int:pk>/update/", views.PostUpdateView.as_view(), name='post-update'),
+    path("post/<int:pk>/delete/", views.PostDeleteView.as_view(), name='post-delete'),
 ]
+
+[, "post/<int:pk>/update/", "post/new/", "post/<int:pk>/delete/", "posts/<int:pk>/", "posts/"]
