4ce9bdfa080857e8325b099c8e9e9c3701a0759b
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-10-05 22:43:12 +0100

    fixing authentication

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic.detail import DetailView
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
 
-# Create your views here.
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth import login
+from django.contrib import messages
+
+
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'blog/register.html'
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()  # creates the user
+            messages.success(request, "Account created successfully. You can now log in.")
+            return redirect("login")  # or wherever you want to redirect
+    else:
+        form = UserCreationForm()
+
+    return render(request, "blog/register.html", {"form": form})
+
+
+class CustomLoginView(LoginView):
+    template_name = 'blog/login.html'
+
+class CustomLogoutView(LogoutView):
+    template_name = 'blog/logout.html'
