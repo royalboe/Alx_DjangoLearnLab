deac2ad0c22e2334c3d9e6fc2ae7f6ce29bbd4fa
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-10-05 23:03:34 +0100

    fixed typo

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -18,6 +18,6 @@ urlpatterns = [
     path('posts/', views.PostListView.as_view(), name='post-list'),
     path('posts/new/', views.PostCreateView.as_view(), name='post-create'),
     path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
-    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-update'),
+    path('posts/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
     path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 ]
