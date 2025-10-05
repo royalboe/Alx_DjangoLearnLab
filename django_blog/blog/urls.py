df190a243ca9326e360b75835467579507eea0bb
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-10-05 22:45:49 +0100

    working on auth

---
+++ django_blog/blog/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from .views import SignUpView, register, CustomLoginView, CustomLogoutView
+import views
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
+
+urlpatterns = [
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('register/', register, name='signup'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('register/', views.register, name='signup'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+
+]
