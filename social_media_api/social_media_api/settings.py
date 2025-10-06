81ee0c23690537c9bfe7389a4baa2ceee0db4cf1
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-10-06 11:24:33 +0100

    added djoser for registration

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'accounts'
+    
+    'accounts',
+    'djoser',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +125,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+RESTFRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+}
+AUTH_USER_MODEL = 'accounts.User'
