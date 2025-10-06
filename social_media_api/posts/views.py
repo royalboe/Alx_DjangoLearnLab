42f1fb1aa846d117fca78bbaaa91d5a17792bb24
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-10-06 20:54:04 +0100

    added the views

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+# Step 3: Create Views for CRUD Operations
+# View Implementation:
+# Using Django REST Framework’s viewsets, set up CRUD operations for both posts and comments in posts/views.py.
+# Implement permissions to ensure users can only edit or delete their own posts and comments.
 
-# Create your views here.
+
+from rest_framework import filters
+from rest_framework import viewsets, permissions
+from .models import Post, Comment
+from .filters import PostFilter, CommentFilter
+
+from .serializers import PostSerializer, CommentSerializer
+from .permissions import IsOwnerOrReadOnly
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['title', 'content']
+    ordering_fields = ['created_at', 'updated_at']
+    ordering = ['-created_at']
+    filterset_class = PostFilter
+    # permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            return [permissions.AllowAny()]
+        elif self.action in ['create']:
+            return [permissions.IsAuthenticated()]
+        elif self.action in ['update', 'partial_update', 'destroy']:
+            return [permissions.IsAuthenticated(), IsOwnerOrReadOnly()]
+        return super().get_permissions()
+    
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['title', 'content']
+    ordering_fields = ['created_at', 'updated_at']
+    ordering = ['-created_at']
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
