f309e7c0165b4731286f66f11997867fbda177da
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-10-07 14:12:19 +0100

    notification model

--- social_media_api/notifications/models.py
+++ social_media_api/notifications/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+
+User = settings.AUTH_USER_MODEL
 
 # Create your models here.
+
+class Notification(models.Model):
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
+    actor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='actor_notifications')
+    verb = models.CharField(max_length=255)
+    target_content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, null=True, blank=True)
+    target_object_id = models.CharField(null=True, blank=True)
+    target = models.GenericForeignKey('target_content_type', 'target_object_id')
+
+    unread = models.BooleanField(default=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-timestamp',)
+
+    def __str__(self):
+        return f'Notification for {self.recipient.username} - {self.verb}'
