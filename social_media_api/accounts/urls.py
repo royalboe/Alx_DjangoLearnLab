59b9da455d7ab0897d64178d62e127f1a1d7fa3c
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-10-07 11:49:31 +0100

    requirment changes

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -4,9 +4,9 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 
-router.register(r'/register', views.RegisterViewSet, basename='register')
-router.register(r'/login', views.LoginViewSet, basename='login')
-router.register(r'/profile', views.ProfileViewSet, basename='profile')
+router.register(r'register/', views.RegisterViewSet, basename='register')
+router.register(r'login/', views.LoginViewSet, basename='login')
+router.register(r'profile/', views.ProfileViewSet, basename='profile')
 
 urlpatterns = {
     path('', include(router.urls)),
