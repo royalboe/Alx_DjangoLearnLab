040bb7855748deb9495c1c7b5ea27cbae6f59938
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-10-07 11:21:43 +0100

    added the feeds url

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -3,11 +3,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import PostViewSet, CommentViewSet
+from .views import PostViewSet, CommentViewSet, FeedViewSet
 
 router = DefaultRouter()
-router.register(r'posts', PostViewSet, basename='post')
-router.register(r'comments', CommentViewSet, basename='comment')
+router.register('posts', PostViewSet, basename='post')
+router.register('comments', CommentViewSet, basename='comment')
+router.register("feed", FeedViewSet, basename='feed')
 
 urlpatterns = [
     path('', include(router.urls)),
