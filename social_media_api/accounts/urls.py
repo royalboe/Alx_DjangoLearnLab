7d3240a099f295a552b43fe7df5b5dc131e8a734
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-10-07 11:23:04 +0100

    added the urls for follow and unfollow

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from rest_framework.routers import DefaultRouter
 
@@ -7,5 +7,11 @@ router = DefaultRouter()
 router.register(r'register', views.RegisterViewSet, basename='register')
 router.register(r'login', views.LoginViewSet, basename='login')
 router.register(r'profile', views.ProfileViewSet, basename='profile')
+
+urlpatterns = {
+    path('', include(router.urls)),
+    path('follow/<int:user_id>/', views.follow_user, name='follow_user'),
+    path('unfollow/<int:user_id>/', views.unfollow_user, name='unfollow_user'),
+}
 
 urlpatterns = router.urls
